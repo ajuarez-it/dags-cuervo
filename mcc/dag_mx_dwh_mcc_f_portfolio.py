05e5183352727410083a7c21c3dbb3ad3843cfde
sancjes <armando.sanchez@gtim.mx>
2025-09-30 14:21:51 -0600

    reducing code in less complex facts as budget, portfolio and sell out

--- mcc/dag_mx_dwh_mcc_f_portfolio.py
+++ mcc/dag_mx_dwh_mcc_f_portfolio.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from airflow.models.dag import DAG
 from airflow.operators.empty import EmptyOperator # Import EmptyOperator
 from airflow.providers.google.cloud.operators.cloud_run import CloudRunExecuteJobOperator
+from airflow.utils.task_group import TaskGroup
 from utils import get_current_filename_base
 
 # ---
@@ -24,56 +25,20 @@ with DAG(
     schedule_interval=None,
     catchup=False,
     tags=["MCC", "PORTFOLIO", "SILVER", "GOLD"],
-    description="A DAG to trigger portfolio Workflow with Qlik, BigQuery, and Cloud Run jobs.",
+    description="A DAG to trigger portfolio Workflow with BigQuery, and Cloud Run jobs.",
 ) as dag:
-
+    default_cloudrun_args = {
+        "project_id": GCP_PROJECT_ID,
+        "region": GCP_REGION,
+        "job_name": JOB_NAME,
+        "gcp_conn_id": GCP_CONN_ID,
+    }
     # ---
     # 3. Task Definitions
     # ---
     start = EmptyOperator(task_id="start")
-    # Job 1: 
-    trigger_cloud_run_job_for_silver_portfolio = CloudRunExecuteJobOperator(
-        task_id="trigger_cloud_run_job_for_silver_portfolio",
-        project_id=GCP_PROJECT_ID,
-        region=GCP_REGION,
-        job_name=JOB_NAME, # Name of the Cloud Run Job
-        gcp_conn_id=GCP_CONN_ID,
-        overrides={
-            "container_overrides": [
-                {
-                    "name": JOB_NAME, # Must match the job name
-                    "args": ["run", "--select", "dbt_cuervo.staging.portfolio"]
-                }
-            ]
-        },
-        doc_md="Triggers the Cloud Run job with overrides for the NA region."
-    )
-
-    # Job 2: 
-    trigger_cloud_run_job_for_gold_portfolio = CloudRunExecuteJobOperator(
-        task_id="trigger_cloud_run_job_for_gold_portfolio",
-        project_id=GCP_PROJECT_ID,
-        region=GCP_REGION,
-        job_name=JOB_NAME,
-        gcp_conn_id=GCP_CONN_ID,
-        overrides={
-            "container_overrides": [
-                {
-                    "name": JOB_NAME,
-                    "args": ["run", "--select", "marts.commercial.f_mcc_portfolio"]
-                }
-            ]
-        },
-        doc_md="Triggers the Cloud Run job with overrides for gold stage in portfolio"
-    )
-
-    # Job 3: 
-    trigger_cloud_run_job_test_bronze_portfolio = CloudRunExecuteJobOperator(
+    TG_bronze = CloudRunExecuteJobOperator(
         task_id="trigger_cloud_run_job_test_bronze_portfolio",
-        project_id=GCP_PROJECT_ID,
-        region=GCP_REGION,
-        job_name=JOB_NAME,
-        gcp_conn_id=GCP_CONN_ID,
         overrides={
             "container_overrides": [
                 {
@@ -82,54 +47,73 @@ with DAG(
                 }
             ]
         },
-        doc_md="Triggers the Cloud Run job with overrides for testing.",
+        doc_md="Triggers the Cloud Run job for testing bronze layer",
+        **default_cloudrun_args,
     )
+    with TaskGroup("TG_silver") as TG_silver:
+        with TaskGroup("test") as silver_test:
+                trigger_cloud_run_job_test_silver_portfolio = CloudRunExecuteJobOperator(
+                    task_id="trigger_cloud_run_job_test_silver_portfolio",
+                    overrides={
+                        "container_overrides": [
+                            {
+                                "name": JOB_NAME, # Must match the job name
+                                "args": ["test", "--select", "dbt_cuervo.staging.portfolio"]
+                            }
+                        ]
+                    },
+                    doc_md="Triggers the Cloud Run job for testing silver layer"
+                    **default_cloudrun_args,
+                )
+        with TaskGroup("run") as silver_run:
+                trigger_cloud_run_job_for_silver_portfolio = CloudRunExecuteJobOperator(
+                    task_id="trigger_cloud_run_job_for_silver_portfolio",
+                    overrides={
+                        "container_overrides": [
+                            {
+                                "name": JOB_NAME,
+                                "args": ["run", "--select", "dbt_cuervo.staging.portfolio"],
+                            }
+                        ]
+                    },
+                    doc_md="Triggers the Cloud Run job for running silver layer",
+                    **default_cloudrun_args,
+                )
+        trigger_cloud_run_job_test_silver_portfolio >> trigger_cloud_run_job_for_silver_portfolio
+    silver_test >> silver_run
+
+    with TaskGroup("TG_gold") as TG_gold:
+        with TaskGroup("test") as gold_test:
+                trigger_cloud_run_job_test_gold_portfolio = CloudRunExecuteJobOperator(
+                    task_id="trigger_cloud_run_job_test_gold_portfolio",
+                    overrides={
+                        "container_overrides": [
+                            {
+                                "name": JOB_NAME, # Must match the job name
+                                "args": ["test", "--select", "dbt_cuervo.marts.commercial.f_mcc_portfolio"]
+                            }
+                        ]
+                    },
+                    doc_md="Triggers the Cloud Run job for testing gold layer"
+                    **default_cloudrun_args,
+                )
+        with TaskGroup("run") as gold_run:
+                trigger_cloud_run_job_for_gold_portfolio = CloudRunExecuteJobOperator(
+                    task_id="trigger_cloud_run_job_for_gold_portfolio",
+                    overrides={
+                        "container_overrides": [
+                            {
+                                "name": JOB_NAME,
+                                "args": ["run", "--select", "dbt_cuervo.marts.commercial.f_mcc_portfolio"],
+                            }
+                        ]
+                    },
+                    doc_md="Triggers the Cloud Run job for running gold layer",
+                    **default_cloudrun_args,
+                )
+        trigger_cloud_run_job_test_gold_portfolio >> trigger_cloud_run_job_for_gold_portfolio
+    gold_test >> gold_run
+
     end = EmptyOperator(task_id="end")
-     # Job 4: 
-    trigger_cloud_run_job_test_silver_portfolio = CloudRunExecuteJobOperator(
-        task_id="trigger_cloud_run_job_test_silver_portfolio",
-        project_id=GCP_PROJECT_ID,
-        region=GCP_REGION,
-        job_name=JOB_NAME,
-        gcp_conn_id=GCP_CONN_ID,
-        overrides={
-            "container_overrides": [
-                {
-                    "name": JOB_NAME,
-                    "args": ["test", "--select", "dbt_cuervo.staging.portfolio"]
-                }
-            ]
-        },
-        doc_md="Triggers the Cloud Run job with overrides for testing."
-    )
-    end = EmptyOperator(task_id="end")
-     # Job 5: 
-    trigger_cloud_run_job_test_gold_portfolio = CloudRunExecuteJobOperator(
-        task_id="trigger_cloud_run_job_test_gold_portfolio",
-        project_id=GCP_PROJECT_ID,
-        region=GCP_REGION,
-        job_name=JOB_NAME,
-        gcp_conn_id=GCP_CONN_ID,
-        overrides={
-            "container_overrides": [
-                {
-                    "name": JOB_NAME,
-                    "args": ["test", "--select", "dbt_cuervo.marts.commercial.f_mcc_portfolio"],
-                }
-            ]
-        },
-        doc_md="Triggers the Cloud Run job for testing gold layer",
-    )
-    # ---
-    # 4. Task Dependencies
-    # ---
-    # Defines the execution order of the tasks in the DAG.
-(
-    start
-    >> trigger_cloud_run_job_test_bronze_portfolio
-    >> trigger_cloud_run_job_test_silver_portfolio
-    >> trigger_cloud_run_job_for_silver_portfolio
-    >> trigger_cloud_run_job_test_gold_portfolio
-    >> trigger_cloud_run_job_for_gold_portfolio
-    >> end
-)
+
+start >> TG_bronze >> TG_silver >> TG_gold >> end
--- mcc/dag_mx_dwh_mcc_f_sell_out.py
+++ mcc/dag_mx_dwh_mcc_f_sell_out.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from airflow.models.dag import DAG
 from airflow.operators.empty import EmptyOperator # Import EmptyOperator
 from airflow.providers.google.cloud.operators.cloud_run import CloudRunExecuteJobOperator
+from airflow.utils.task_group import TaskGroup
 from utils import get_current_filename_base
 
 # ---
@@ -24,107 +25,95 @@ with DAG(
     schedule_interval=None,
     catchup=False,
     tags=["MCC", "SELL_OUT", "SILVER", "GOLD"],
-    description="A DAG to transform data from silver to gold",
+    description="A DAG to trigger sell_out Workflow with BigQuery, and Cloud Run jobs.",
 ) as dag:
-
+    default_cloudrun_args = {
+        "project_id": GCP_PROJECT_ID,
+        "region": GCP_REGION,
+        "job_name": JOB_NAME,
+        "gcp_conn_id": GCP_CONN_ID,
+    }
+    # ---
+    # 3. Task Definitions
+    # ---
     start = EmptyOperator(task_id="start")
-    # Step 1: execute all the transformations from gold to silver 
-    trigger_cloud_run_job_for_silver_sell_out = CloudRunExecuteJobOperator(
-        task_id="trigger_cloud_run_job_for_silver_sell_out",
-        project_id=GCP_PROJECT_ID,
-        region=GCP_REGION,
-        job_name=JOB_NAME, # Name of the Cloud Run Job
-        gcp_conn_id=GCP_CONN_ID,
-        overrides={
-            "container_overrides": [
-                {
-                    "name": JOB_NAME, # Must match the job name
-                    "args": ["run", "--select", "dbt_cuervo.staging.sell_out"],
-                }
-            ]
-        },
-        doc_md="Triggers the Cloud Run job with overrides for the NA region.",
-    )
-    # Step 2: execute the fact table in gold 
-    trigger_cloud_run_job_for_gold_sell_out = CloudRunExecuteJobOperator(
-        task_id="trigger_cloud_run_job_for_gold_sell_out",
-        project_id=GCP_PROJECT_ID,
-        region=GCP_REGION,
-        job_name=JOB_NAME,
-        gcp_conn_id=GCP_CONN_ID,
-        overrides={
-            "container_overrides": [
-                {
-                    "name": JOB_NAME,
-                    "args": ["run", "--select", "dbt_cuervo.marts.commercial.f_mcc_sell_out"],
-                }
-            ]
-        },
-        doc_md="Triggers the Cloud Run job with overrides for gold stage in sellout",
-    )
-
-    trigger_cloud_run_job_test_bronze_sell_out = CloudRunExecuteJobOperator(
+    TG_bronze = CloudRunExecuteJobOperator(
         task_id="trigger_cloud_run_job_test_bronze_sell_out",
-        project_id=GCP_PROJECT_ID,
-        region=GCP_REGION,
-        job_name=JOB_NAME,
-        gcp_conn_id=GCP_CONN_ID,
         overrides={
             "container_overrides": [
                 {
                     "name": JOB_NAME,
-                    "args": ["test", "--select", "source:dbt_cuervo.BRZ_MX_ONP_SAP_BW.raw_zcprt001_q0006"],
+                    "args": ["test", "--select", "source:dbt_cuervo.BRZ_MX_ONP_SAP_BW.raw_zcprt001_q0006"]
                 }
             ]
         },
-        doc_md="Triggers the Cloud Run job for testing bronze layer",
+        doc_md="Triggers the Cloud Run job with overrides for testing.",
+        **default_cloudrun_args,
     )
-    
-    trigger_cloud_run_job_test_silver_sell_out = CloudRunExecuteJobOperator(
-        task_id="trigger_cloud_run_job_test_silver_sell_out",
-        project_id=GCP_PROJECT_ID,
-        region=GCP_REGION,
-        job_name=JOB_NAME,
-        gcp_conn_id=GCP_CONN_ID,
-        overrides={
-            "container_overrides": [
-                {
-                    "name": JOB_NAME,
-                    "args": ["test", "--select", "dbt_cuervo.staging.sell_out"],
-                }
-            ]
-        },
-        doc_md="Triggers the Cloud Run job for testing silver layer",
-    )
+    with TaskGroup("TG_silver") as TG_silver:
+        with TaskGroup("test") as silver_test:
+                trigger_cloud_run_job_test_silver_sell_out = CloudRunExecuteJobOperator(
+                    task_id="trigger_cloud_run_job_test_silver_sell_out",
+                    overrides={
+                        "container_overrides": [
+                            {
+                                "name": JOB_NAME, # Must match the job name
+                                "args": ["test", "--select", "dbt_cuervo.staging.sell_out"]
+                            }
+                        ]
+                    },
+                    doc_md="Triggers the Cloud Run job for testing silver layer"
+                    **default_cloudrun_args,
+                )
+        with TaskGroup("run") as silver_run:
+                trigger_cloud_run_job_for_silver_sell_out = CloudRunExecuteJobOperator(
+                    task_id="trigger_cloud_run_job_for_silver_sell_out",
+                    overrides={
+                        "container_overrides": [
+                            {
+                                "name": JOB_NAME,
+                                "args": ["run", "--select", "dbt_cuervo.staging.sell_out"],
+                            }
+                        ]
+                    },
+                    doc_md="Triggers the Cloud Run job for running silver layer",
+                    **default_cloudrun_args,
+                )
+        trigger_cloud_run_job_test_silver_sell_out >> trigger_cloud_run_job_for_silver_sell_out
+    silver_test >> silver_run
 
-    trigger_cloud_run_job_test_gold_sell_out = CloudRunExecuteJobOperator(
-        task_id="trigger_cloud_run_job_test_gold_sell_out",
-        project_id=GCP_PROJECT_ID,
-        region=GCP_REGION,
-        job_name=JOB_NAME,
-        gcp_conn_id=GCP_CONN_ID,
-        overrides={
-            "container_overrides": [
-                {
-                    "name": JOB_NAME,
-                    "args": ["test", "--select", "dbt_cuervo.marts.commercial.f_mcc_sell_out"],
-                }
-            ]
-        },
-        doc_md="Triggers the Cloud Run job for testing gold layer",
-    )
+    with TaskGroup("TG_gold") as TG_gold:
+        with TaskGroup("test") as gold_test:
+                trigger_cloud_run_job_test_gold_sell_out = CloudRunExecuteJobOperator(
+                    task_id="trigger_cloud_run_job_test_gold_sell_out",
+                    overrides={
+                        "container_overrides": [
+                            {
+                                "name": JOB_NAME, # Must match the job name
+                                "args": ["test", "--select", "dbt_cuervo.marts.commercial.f_mcc_sell_out"]
+                            }
+                        ]
+                    },
+                    doc_md="Triggers the Cloud Run job for testing gold layer"
+                    **default_cloudrun_args,
+                )
+        with TaskGroup("run") as gold_run:
+                trigger_cloud_run_job_for_gold_sell_out = CloudRunExecuteJobOperator(
+                    task_id="trigger_cloud_run_job_for_gold_sell_out",
+                    overrides={
+                        "container_overrides": [
+                            {
+                                "name": JOB_NAME,
+                                "args": ["run", "--select", "dbt_cuervo.marts.commercial.f_mcc_sell_out"],
+                            }
+                        ]
+                    },
+                    doc_md="Triggers the Cloud Run job for running gold layer",
+                    **default_cloudrun_args,
+                )
+        trigger_cloud_run_job_test_gold_sell_out >> trigger_cloud_run_job_for_gold_sell_out
+    gold_test >> gold_run
+
     end = EmptyOperator(task_id="end")
 
-    # ---
-    # 4. Task Dependencies
-    # ---
-    # Defines the execution order of the tasks in the DAG.
-(
-    start
-    >> trigger_cloud_run_job_test_bronze_sell_out
-    >> trigger_cloud_run_job_test_silver_sell_out
-    >> trigger_cloud_run_job_for_silver_sell_out
-    >> trigger_cloud_run_job_test_gold_sell_out
-    >> trigger_cloud_run_job_for_gold_sell_out
-    >> end
-)
+start >> TG_bronze >> TG_silver >> TG_gold >> end
